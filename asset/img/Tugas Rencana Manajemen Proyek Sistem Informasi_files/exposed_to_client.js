189cf79027d6437192a29ad79f3ea1ba3e942dfd
yuan putra <yuan putra>
2016-05-26 11:53:07 +0700

    this first

---
+++ asset/img/Tugas Rencana Manajemen Proyek Sistem Informasi_files/exposed_to_client.js
@@ -0,0 +1,9 @@
+
+      if(!window.Scribd) Scribd = {};
+
+      Scribd.ServerOptions = {"ad_refresh_engagement_tracking_delay":1200,"ad_refresh_idle_time_before_inactive":60,"ad_refresh_interval":60,"disable_trending":false,"facebook_autologin_enabled":false,"library_dropdown_test_choices":["control","test"],"library_dropdown_test_enabled":false,"payments_store_disabled":false,"reading_history_scroll_timeout":500,"seo_roadblock_choices":["question_promo","promo","banner"],"seo_roadblock_enabled":true,"seo_roadblock_rollout":1.0,"shadow_autocomplete_enabled":false,"sidebar_promo_enabled":false};
+
+      Scribd.NwayTests = {};
+
+      Scribd.NwayTestsWithWeights = {"home_app_promo":{"id":381,"choices":{"954":{"name":"control","weight":1},"955":{"name":"a","weight":1},"956":{"name":"b","weight":1},"957":{"name":"c","weight":1}}},"search_app_cookie":{"id":422,"choices":{"1041":{"name":"test","weight":50},"1042":{"name":"control","weight":50}}},"doc_page_ads":{"id":423,"choices":{"1043":{"name":"enable","weight":1},"1044":{"name":"disable","weight":1}}},"seo_banner":{"id":428,"choices":{"1057":{"name":"control","weight":1},"1058":{"name":"test1","weight":1},"1059":{"name":"test2","weight":1}}}};
+    
---
+++ asset/js/yuan.js
@@ -0,0 +1,13 @@
+function get(data){
+    	var url=window.location.href, res='';
+    	url=url.split('?')[1]+'';
+ 		url=url.split('&');
+ 		for(var i=0;i<url.length;i++){
+ 			url[i]=''+url[i];
+ 			url[i]=url[i].split('=');
+ 			if(url[i][0]==data){
+ 				res=url[i][1];
+ 			} 			
+ 		}
+ 			return res;
+ 		}
