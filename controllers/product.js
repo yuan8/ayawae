69eff388531106ffffc35902bbdec242f99e87b5
yuan putra <yuan putra>
2016-05-27 07:05:00 +0700

    ini

--- controllers/product.js
+++ controllers/product.js
@@ -1,1 +1,16 @@
-console.log(''aku masuk ni);
+var dataPage={
+
+	page:'product',
+	category:getq('category'),
+	npage:getq('npage')
+
+
+}
+
+
+
+for(var i=0;i<6;i++){
+
+$('.box-goods').append('<a href="./?page=detailproduct" style="color:#222;">'+$('.goods').clone().html()+'</a>');
+
+}
