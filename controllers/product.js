17a7ca03625eb26c1b74486e367e8fa1388f5c93
yuan.putra@windowslive.com <yuan.putra@windowslive.com>
2016-05-28 20:02:27 +0700

    no message

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,17 @@
+
+$.getScript( "asset/plugin/yu-session/yu-session.js", function(data){});
+
+
+
 var dataPage={
 
 	page:'product',
 	category:getq('category'),
-	npage:getq('npage')
-
+	npage:getq('npage'),
+	login_first:false
 
 }
+
 
 
 
@@ -13,4 +19,6 @@ for(var i=0;i<6;i++){
 
 $('.box-goods').append('<a href="./?page=detailproduct" style="color:#222;">'+$('.goods').clone().html()+'</a>');
 
+
 }
+
